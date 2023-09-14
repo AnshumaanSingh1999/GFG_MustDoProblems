73f2c3e667edb71f67fb8fc0142e77ac31a006b6
Anshumaan Singh <anshumaanborntolead@gmail.com>
2023-09-14 21:06:22 +0530

    Initial commit

---
+++ KeyPair.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class KeyPair {
+    static boolean test(int a[],int n,int s){
+        boolean d=false;
+        int c=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j)
+                {
+                    continue;
+                }
+                else{
+                    c=a[i]+a[j];
+                    if(c==s){
+                        d=true;
+                    } 
+                }
+            }
+                
+        }
+        return d;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        boolean flag=test(a,n,s);
+        String result=flag?"Yes":"No";
+        System.out.println(result);
+    }
+}

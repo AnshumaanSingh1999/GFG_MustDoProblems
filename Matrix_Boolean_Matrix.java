e29f7c19aa476c5ed41262745ca11664c47107c2
Anshumaan Singh <anshumaanborntolead@gmail.com>
2023-09-15 14:55:10 +0530

    Update Matrix_Boolean_Matrix.java

--- Matrix_Boolean_Matrix.java
+++ Matrix_Boolean_Matrix.java
@@ -1,26 +1,35 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Matrix_Boolean_Matrix {
     static void booleanMatrix(int a[][]){
         int r=a.length;
         int c=a[0].length;
-        int row[]=new int[r];
-        int col[]=new int[c];
-        for (int k = 0; k < r; k++)
-            row[k] = 0;
-        for (int l = 0; l < c; l++)
-            col[l] = 0;
+        // int row[]=new int[r];
+        // int col[]=new int[c];
+        // for (int k = 0; k < r; k++)
+        //     row[k] = 0;
+        // for (int l = 0; l < c; l++)
+        //     col[l] = 0;
+        ArrayList<Integer> rows=new ArrayList<Integer>();
+        ArrayList<Integer> cols=new ArrayList<Integer>();
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(a[i][j]==1){
-                    row[i]=1;
-                    col[j]=1;
+                    rows.add(i);
+                    cols.add(j);
+                    // row[i]=1;
+                    // col[j]=1;
                 }
             }
         }
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(row[i]==1 || col[j]==1){
-                    a[i][j]=1;
+                for(int x:rows){
+                    for(int y:cols){
+                        if(i==x || j==y){
+                            a[i][j]=1;
+                        }
+                    }
                 }
             }
         }

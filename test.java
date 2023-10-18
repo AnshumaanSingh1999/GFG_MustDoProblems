896728296c3dcadaa494e4b285371776799820b9
Anshumaan Singh <anshumaanborntolead@gmail.com>
2023-10-18 13:01:41 +0530

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        double a=1;
-        double b=2;
-        double s=(a+b)/2;
-        System.out.println(s);
-    }
-    
-}
